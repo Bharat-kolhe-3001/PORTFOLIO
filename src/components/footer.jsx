0ba1f94b58a1c11febdfd86a5ff183e13e8685c4
Bharat kolhe <163564571+Bharat-kolhe-3001@users.noreply.github.com>
2026-02-11 22:39:47 +0530

    Create footer.jsx

---
+++ src/components/footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../CSS/Footer.css";
+
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>© 2026 Bharat Kolhe. All Rights Reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
